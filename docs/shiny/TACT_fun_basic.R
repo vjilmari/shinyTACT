34ce4307721b7e3941534ff92f7a2d48af9b8acb
vjilmari <vj.ilmarinen@gmail.com>
2023-10-22 16:08:19 +0300

    Add advanced mode

---
+++ docs/shiny/TACT_fun_basic.R
@@ -0,0 +1,247 @@
+TACT_fun_basic <- function(r=NULL, x=NULL, y=NULL, distribution = c("normal", "uniform", "skewed"),
+                           n = 10^6,
+                     cutoffsx = c(1/3,2/3), cutoffsy = c(1/3,2/3),
+                     plot=TRUE, n.plotted = 10^3, Main = "",
+                     Xlab = "X", Ylab = "Y",
+                     #Char=c(-0x263A,-0x2639,-0x2639,-0x2639,-0x263A,-0x2639,-0x2639,-0x2639,-0x263A),
+                     #Char = c("\u2713", "\u2717", "\u2717", "\u2717", "\u2713", "\u2717", "\u2717", "\u2717", "\u2713"),
+                     #Col=c("olivedrab2","orangered3","orangered3","orangered3","olivedrab2",
+                     #       "orangered3","orangered3","orangered3","olivedrab2"),
+                     #Cex=c(1.2,0.9,0.9,0.9,1.2,0.9,0.9,0.9,1.2),
+                     
+                     #Char=c(-0x2639,-0x2639,-0x263A,-0x2639,-0x263A,-0x2639,-0x263A,-0x2639,-0x2639),
+                     Char=c("\u2717","\u2717","\u2713",
+                            "\u2717","\u2713","\u2717",
+                            "\u2713","\u2717","\u2717"),
+                     Col=c("orangered3","orangered3","olivedrab2",
+                           "orangered3","olivedrab2","orangered3",
+                           "olivedrab2","orangered3","orangered3"),
+                     Cex=c(0.9,0.9,1.2,0.9,1.2,0.9,1.2,0.9,0.9),
+                     font_size = 1,
+                     plot.percents = c(TRUE,TRUE,TRUE,
+                                       TRUE,TRUE,TRUE,
+                                       TRUE,TRUE,TRUE),
+                     language="fi",
+                     Cex_multiplier = 1)
+
+{
+  #r = .80
+  #distribution = "normal"
+  #n = 10^6
+  #cutoffsx = c(1,2,3,4)/4
+  #cutoffsy = c(1,2,3,4)/4
+  cutoffsx = c(1/3,2/3)
+  cutoffsy = c(1/3,2/3)
+  #plot.percents = c(TRUE,TRUE,TRUE,TRUE,TRUE, TRUE,TRUE,TRUE,TRUE)
+  #plot=TRUE
+  #n.plotted = 10^3
+  #Xlab = "X"
+  #Ylab = "Y"
+  #Char=c("\u2717","\u2717","\u2713",
+  #       "\u2717","\u2713","\u2717",
+  #       "\u2713","\u2717","\u2717")
+  #Col=c("orangered3","orangered3","olivedrab2",
+  #      "orangered3","olivedrab2","orangered3",
+  #      "olivedrab2","orangered3","orangered3")
+  #Cex=c(0.9,0.9,1.2,0.9,1.2,0.9,1.2,0.9,0.9)
+  #Main = ""
+  #font_size = 1
+  #Cex_multiplier=1
+  
+  cut3 <- function(x,c1,c2) cut(x,quantile(x,c(0,c1,c2,1)),
+                                c("Low","Medium","High"), include.lowest=TRUE)
+  
+  # function to make the percentages symmetric
+  harmonize <- function(x){
+    val <- (x[lower.tri(x)] + t(x)[lower.tri(x)]) / 2
+    x[lower.tri(x)] = val
+    x <- t(x)
+    x[lower.tri(x)] = val
+    x
+  }
+  
+  if( !is.null(r) ) {
+    # test if variables are only cut in two
+    if(diff(cutoffsx) == 0) cutoffsx[1] <- cutoffsx[1] + 1/10^8
+    if(diff(cutoffsy) == 0) cutoffsy[1] <- cutoffsy[1] + 1/10^8
+    
+    # generate normal y from normal x with linear model
+    if(distribution == "normal"){
+      x <- rnorm(n)
+      y <- r * x + sqrt(1 - r^2)*rnorm(n)
+    }
+    if(distribution == "uniform"){
+      x <- runif(n, -1, 1)
+      y <- r * x + sqrt(1 - r^2)*runif(n,-1,1)
+    }
+    if(distribution == "skewed"){
+      x <- rbeta(n, 5, 1)
+      y <- r * x + sqrt(1 - r^2)*rbeta(n, 5, 1)
+    }
+    
+  } else {
+    val <- !is.na(x) & !is.na(y)
+    y <- y[val]
+    x <- x[val]
+    if(sum(!val) > 0) warning("\nMissing values deleted\n")
+    n <- length(x)
+  }
+  
+  # allocate observations to tabs based on cutoffs
+  tabs <- data.frame(y = cut3(y, cutoffsy[1], cutoffsy[2]),
+                     x = cut3(x, cutoffsx[1], cutoffsx[2]))
+  # calculate percentages in each tab
+  crosstabs <- apply(prop.table(table(tabs)),2,function(x) x/sum(x))
+  
+  # balance the proportions at low/low and high/high to be the same
+  dr = (crosstabs[1,1] + crosstabs[3,3]) / 2
+  # harmonize the proportions
+  if((1/3) %in% cutoffsx & (2/3) %in% cutoffsx & (1/3) %in% cutoffsy & (2/3) %in% cutoffsy ) {
+    crosstabs[1,1] <- dr
+    crosstabs[3,3] <- dr
+    crosstabs <- harmonize(crosstabs)
+  }
+  # name rows and columns
+  crosstabs <- crosstabs[c("High","Medium","Low"),
+                         c("Low","Medium","High")]
+  # convert to percentages
+  percents <- matrix(paste(round((crosstabs * 100),1),"%", sep=""), ncol=3)
+  # request only certain percentages to be plotted
+  percents[!plot.percents] <- ""
+  # correlation between x and y
+  cr <- cor(x,y)
+  xorig <- x
+  yorig <- y
+  
+  # xorig[xorig < -4.5] = 4.5
+  # xorig[xorig > 4.5] = 4.5
+  # yorig[yorig < -4.5] = 4.5
+  # yorig[yorig > 4.5] = 4.5
+  if(plot){
+    if(n > n.plotted){
+      # draw a subsample from the original
+      samp <- sample(n,n.plotted)
+      x <- x[samp]
+      y <- y[samp]
+      tabssamp <- data.frame(y = cut3(y, cutoffsy[1], cutoffsy[2]),
+                             x = cut3(x, cutoffsx[1], cutoffsx[2]))
+      # assign a coloring scheme for the categories
+      color <- as.factor(apply(tabssamp, 1, paste, collapse=""))
+      
+    } else {
+      color = as.factor(apply(tabs, 1, paste, collapse=""))
+    }
+    #color <- recode(color, 
+    #                "LowHigh"=9,"LowMedium"=6,"LowLow"=3,
+    #                "MediumHigh"=8,"MediumMedium"=5,"MediumLow"=2,
+    #                "HighHigh"=7,"HighMedium"=4,"HighLow"=1
+    #)
+    
+    if (r >= 0){
+      color <- recode(color, 
+                      "LowHigh"=9,"LowMedium"=6,"LowLow"=3,
+                      "MediumHigh"=8,"MediumMedium"=5,"MediumLow"=2,
+                      "HighHigh"=7,"HighMedium"=4,"HighLow"=1)
+      
+    } else {
+      color <- recode(color, 
+                      "LowHigh"=3,"LowMedium"=6,"LowLow"=9,
+                      "MediumHigh"=8,"MediumMedium"=5,"MediumLow"=2,
+                      "HighHigh"=1,"HighMedium"=4,"HighLow"=7)
+      
+    }
+    
+    
+    plot<-plot(x,y, col="white",xaxt="n", yaxt="n",
+               ylim = c(min(yorig), max(yorig)), xlim = c(min(xorig), max(xorig)),
+               xlab=Xlab, ylab=Ylab)
+    points(x, y,
+           cex=Cex[color]*Cex_multiplier,
+           col=Col[color],
+           pch=Char[color]
+    )
+    
+    xq <- quantile(xorig, cutoffsx)
+    yq <- quantile(yorig, cutoffsy)
+    
+    lines(rep(xq[1],2),c(min(yorig) - abs(min(yorig)),max(yorig) + abs(max(yorig))), col="grey50",lty = 2)
+    lines(rep(xq[2],2),c(min(yorig) - abs(min(yorig)),max(yorig) + abs(max(yorig))), col="grey50",lty = 2)
+    lines(c(min(xorig) - abs(min(xorig)),max(xorig) + abs(max(xorig))), rep(yq[1],2), col="grey50",lty = 2)
+    lines(c(min(xorig) - abs(min(xorig)),max(xorig) + abs(max(xorig))), rep(yq[2],2), col="grey50",lty = 2)
+    
+    text((xq[1] + min(xorig))/2, (yq[2] + max(yorig))/2, percents[1,1], cex=font_size, font=2, col="black")
+    text((xq[1] + min(xorig))/2, (yq[1] + min(yorig))/2, percents[3,1], cex=font_size, font=2, col="black")
+    text((xq[2] + max(xorig))/2, (yq[2] + max(yorig))/2, percents[1,3], font=2, cex=font_size, col="black")
+    text((xq[2] + max(xorig))/2, (yq[1] + min(yorig))/2, percents[3,3], font=2, cex=font_size, col="black")
+    
+    if(abs(diff(cutoffsx)) > .001) {
+      text(median(xorig), (yq[2] + max(yorig))/2, percents[1,2], cex=font_size, font=2,col="black")
+      text(median(xorig), (yq[1] + min(yorig))/2, percents[3,2], cex=font_size, font=2, col="black")
+    }
+    if(abs(diff(cutoffsy)) > .001){
+      text((xq[1] + min(xorig))/2, mean(yorig), percents[2,1], cex=font_size, font=2, col="black")
+      text((xq[2] + max(xorig))/2, mean(yorig), percents[2,3], cex=font_size, font=2, col="black")
+    }
+    
+    if(abs(diff(cutoffsx)) > .001 & abs(diff(cutoffsy)) > .001) text(median(xorig), mean(yorig), percents[2,2], cex=font_size, font=2, col="black")
+    
+    xstand <- (xorig-min(xorig))/(max(xorig)-min(xorig))
+    xq1 <- quantile(xstand, cutoffsx)
+    ystand <- (yorig-min(yorig))/(max(yorig)-min(yorig))
+    yq1 <- quantile(ystand, cutoffsy)
+    mtext(Main, side=3, line=2, adj=0)
+    mtext(paste(TACT_languages[TACT_languages$language==language,"low"],
+                ' (<', paste(round(cutoffsx[1]*100,1),'%)', sep="")),
+          side=1, line=1, cex=font_size, adj=(xq1[1])/2.4, col="black")
+    mtext(paste(TACT_languages[TACT_languages$language==language,"high"],
+                ' (>', paste(round(cutoffsx[2]*100,1),'%)', sep="")),
+          side=1, line=1, cex=font_size, adj=(xq1[2] + 1)/1.75, col="black")
+    mtext(paste(TACT_languages[TACT_languages$language==language,"low"],
+                ' (<', paste(round(cutoffsy[1]*100,1),'%)', sep="")),
+          side=2, line=1, cex=font_size, adj=(yq1[1])/2.4)
+    mtext(paste(TACT_languages[TACT_languages$language==language,"high"],
+                ' (>', paste(round(cutoffsy[2]*100,1),'%)', sep="")),
+          side=2, line=1, cex=font_size, adj=(yq1[2] + 1)/1.75 )
+    
+    
+    
+    if(abs(diff(cutoffsy)) > 0.001) mtext(TACT_languages[TACT_languages$language==language,"medium"],
+                                          side=2, line=1, cex=font_size, adj=median(ystand))
+    if(abs(diff(cutoffsx)) > 0.001) mtext(TACT_languages[TACT_languages$language==language,"medium"],
+                                          side=1, line=1, cex=font_size, adj=median(xstand), col="black")
+    
+    
+    mtext(paste(TACT_languages[TACT_languages$language==language,"correlation"],
+                " =", round(cr,2)), side = 1, line=-1.5, adj=0.04, cex=font_size)
+    
+    # comment the top text out completely
+    #if (r >=0){
+    #  if(abs(diff(cutoffsx)) > 0.001 & abs(diff(cutoffsy)) > 0.001 & language!="es")
+    #    mtext(paste(round(sum(diag(t(apply(crosstabs, 2, rev)))) / sum(crosstabs),3)*100,
+    #                TACT_languages[TACT_languages$language==language,"match_1"],
+    #                TACT_languages[TACT_languages$language==language,"match_2"],sep=""),
+    #          side = 3, line=-8.0, adj=0.04, cex=font_size)
+    #  
+    #  if(abs(diff(cutoffsx)) > 0.001 & abs(diff(cutoffsy)) > 0.001 & language=="es")
+    #    mtext(paste("Keskmiselt ",
+    #                round(sum(diag(t(apply(crosstabs, 2, rev)))) / sum(crosstabs),3)*100,
+    #                "% väärtustest kattuvad.\nSeose täieliku puudumise korral kattuks 33.3%", sep=""),
+    #          side = 3, line=-8.0, adj=0.04, cex=font_size)
+    #  
+    #}
+    
+  }
+  
+  #if(abs(diff(cutoffsx)) < 0.001) crosstabs[,2] <- 0
+  #if(abs(diff(cutoffsx)) > 0.001 & abs(diff(cutoffsy)) > 0.001) cat("\n", "Overall accuracy in matching low, medium and high values:", round(sum(diag(t(apply(crosstabs, 2, rev)))) / sum(crosstabs),3), "(against the random-guess baseline of .333)\n")
+  #round(crosstabs,4)
+  
+  results <- list(
+    crosstabs = crosstabs,
+    cutoffsx = cutoffsx,
+    cutoffsy = cutoffsy,
+    plot = plot
+  )
+  return(results)
+}
+
--- docs/shiny/app.R
+++ docs/shiny/app.R
@@ -1,12 +1,10 @@
 library(shiny)
+library(shinyjs)
 library(jsonlite)
 library(dplyr)
 
-source("TACT_fin.R")
+source("TACT_fun_basic.R")
 source("TACT_languages.R")
-#source("text_below_plot.R")
-
-
 
 # import languages
 translations <- list(
@@ -18,149 +16,323 @@ translations <- list(
 
 
 ui <- fluidPage(
-  selectInput("language", "Language:", 
-              choices = c(
-                "Suomi" = "fi",
-                "English" = "en",
-                "Deutsch" = "ge",
-                "Eesti keel" = "es"
-              ),
-              selected = "fi"
-  ),
-  uiOutput("title_panel"),
+  useShinyjs(),  # Initialize shinyjs
+  
+  # Add custom CSS to style the radio button options
   tags$style(HTML("
-    .sidebar {
+    .radio-btn-inline {
+      display: inline-block;
+      margin-right: 10px;
+    }
+  ")),
+  
+  div(id = "radioButtons",
+      radioButtons("mode_choice", "Mode",
+                   c("Basic", "Advanced"),
+                   inline = TRUE  # Display the radio buttons inline
+      )
+  ),
+  
+  # PANEL FOR BASIC MODE
+  conditionalPanel(
+    condition = 'input.mode_choice == "Basic"',
+    selectInput("language_basic", "Language:",
+                choices = c(
+                  "Suomi" = "fi",
+                  "English" = "en",
+                  "Deutsch" = "ge",
+                  "Eesti keel" = "es"
+                ),
+                selected = "fi"
+    ),
+    uiOutput("title_panel_basic"),
+    tags$style(HTML("
+    .sidebar_basic { 
       position: sticky;
       top: 0;
-      height: calc(100vh - 70px); /* Set the height based on your app's layout */
+      height: calc(100vh - 70px);
       overflow-y: auto;
     }
-    #tactPlot {
+    #tactPlot_basic { 
       max-width: 100%;
       width: auto;
       height: auto;
       padding-bottom: 100%;
       position: relative;
     }
-    #tactPlot > .plot-container {
+    #tactPlot_basic > .plot-container { 
       position: absolute;
       width: 100%;
       height: 100%;
     }
-    .text-below-plot {
-    background-color: white; /* Change this to your desired background color */
-    border: 1px solid #000; /* Add a 1px solid black border */
-    border-radius: 5px; /* Add rounded corners */
-  }
+    .text-below-plot_basic { 
+      background-color: white;
+      border: 1px solid #000;
+      border-radius: 5px;
+    }
   ")),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("r", textOutput("slider_label"), min = -1, max = 1, value = 0, step = 0.01),
-      textInput("x_label", textOutput("x_label_text"), value = "X"),
-      textInput("y_label", textOutput("y_label_text"), value = "Y"),
-      sliderInput("font_size", textOutput("font_size_text"), min = 0.1, max = 3, value = 1, step = 0.1),
-      sliderInput("point_size", textOutput("point_size_text"), min = 0.1, max = 3, value = 1, step = 0.1),
-      tags$p(textOutput("tact_info_text"),
-             tags$br(),
-             tags$a("Mõttus, R. (2022). What Correlations Mean for Individual People: A Tutorial for Researchers, Students and the Public. Personality Science, 3, 1–27. https://doi.org/10.5964/ps.7467",
-                    href = "https://doi.org/10.5964/ps.7467",
-                    target = "_blank")
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("r_basic", textOutput("slider_label_basic"), min = -1, max = 1, value = 0, step = 0.01), 
+        textInput("x_label_basic", textOutput("x_label_text_basic"), value = "X"), 
+        textInput("y_label_basic", textOutput("y_label_text_basic"), value = "Y"), 
+        sliderInput("font_size_basic", textOutput("font_size_text_basic"), min = 0.1, max = 3, value = 1, step = 0.1), 
+        sliderInput("point_size_basic", textOutput("point_size_text_basic"), min = 0.1, max = 3, value = 1, step = 0.1), 
+        tags$p(textOutput("tact_info_text_basic"), 
+               tags$br(),
+               tags$a("Mõttus, R. (2022). What Correlations Mean for Individual People: A Tutorial for Researchers, Students and the Public. Personality Science, 3, 1–27. https://doi.org/10.5964/ps.7467",
+                      href = "https://doi.org/10.5964/ps.7467",
+                      target = "_blank")
+        ),
+        class = "sidebar_basic" 
       ),
-      class = "sidebar"
+      mainPanel(
+        plotOutput("tactPlot_basic"), 
+        uiOutput("text_below_plot_basic") 
+      )
+    )
+  )
+  ,
+  # ADVANCED PANEL MODEL
+  conditionalPanel(
+    condition = 'input.mode_choice == "Advanced"',
+    selectInput("language_advanced", "Language:",
+                choices = c(
+                  "Suomi" = "fi",
+                  "English" = "en",
+                  "Deutsch" = "ge",
+                  "Eesti keel" = "es"
+                ),
+                selected = "fi"
     ),
-    mainPanel(
-      plotOutput("tactPlot"),
-      uiOutput("text_below_plot")
+    uiOutput("title_panel_advanced"),
+    tags$style(HTML("
+    .sidebar_advanced { 
+      position: sticky;
+      top: 0;
+      height: calc(100vh - 70px);
+      overflow-y: auto;
+    }
+    #tactPlot_advanced { 
+      max-width: 100%;
+      width: auto;
+      height: auto;
+      padding-bottom: 100%;
+      position: relative;
+    }
+    #tactPlot_advanced > .plot-container { 
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+    .text-below-plot_advanced { 
+      background-color: white;
+      border: 1px solid #000;
+      border-radius: 5px;
+    }
+  ")),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("r_advanced", textOutput("slider_label_advanced"), min = -1, max = 1, value = 0, step = 0.01), 
+        textInput("x_label_advanced", textOutput("x_label_text_advanced"), value = "X"), 
+        textInput("y_label_advanced", textOutput("y_label_text_advanced"), value = "Y"), 
+        sliderInput("font_size_advanced", textOutput("font_size_text_advanced"), min = 0.1, max = 3, value = 1, step = 0.1), 
+        sliderInput("point_size_advanced", textOutput("point_size_text_advanced"), min = 0.1, max = 3, value = 1, step = 0.1), 
+        selectInput("distribution_advanced", "Variable Distribution",
+                    choices = c("normal", "uniform", "skewed"),
+                    selected = "normal"
+        ),
+        numericInput(inputId="n_advanced",
+                     label= "Simulation sample size", value = 1000000, min = 10, max = 1000000000),
+        numericInput(inputId="n.plotted_advanced",
+                     label= "Number of data points plotted", value = 1000, min = 1, max = 100000),
+        tags$p(textOutput("tact_info_text_advanced"), 
+               tags$br(),
+               tags$a("Mõttus, R. (2022). What Correlations Mean for Individual People: A Tutorial for Researchers, Students and the Public. Personality Science, 3, 1–27. https://doi.org/10.5964/ps.7467",
+                      href = "https://doi.org/10.5964/ps.7467",
+                      target = "_blank")
+        ),
+        class = "sidebar_advanced" 
+      ),
+      mainPanel(
+        plotOutput("tactPlot_advanced"), 
+        uiOutput("text_below_plot_advanced") 
+      )
     )
   )
 )
 
-server <- function(input, output, session) {
+server <- function(input, output) {
+  # BASIC server
   selected_language <- reactive({
-    input$language
+    input$language_basic
   })
   
-  output$title_panel <- renderUI({
+  output$title_panel_basic <- renderUI({
     HTML(paste0("<h1>", translations[[selected_language()]]$title, "</h1>"))
   })
   
-  output$slider_label <- renderText({
+  output$slider_label_basic <- renderText({
     translations[[selected_language()]]$r_slider
   })
   
-  output$x_label_text <- renderText({
+  output$x_label_text_basic <- renderText({
     translations[[selected_language()]]$name_x
   })
   
-  output$y_label_text <- renderText({
+  output$y_label_text_basic <- renderText({
     translations[[selected_language()]]$name_y
   })
   
-  output$font_size_text <- renderText({
+  output$font_size_text_basic <- renderText({
     translations[[selected_language()]]$font_prompt
   })
   
-  output$point_size_text <- renderText({
+  output$point_size_text_basic <- renderText({
     translations[[selected_language()]]$point_prompt
   })
   
-  output$tact_info_text <- renderText({
+  output$tact_info_text_basic <- renderText({
     translations[[selected_language()]]$tact_paper_prompt
   })
   
-  output$tactPlot <- renderPlot({
+  output$tactPlot_basic <- renderPlot({
     # Use the chosen labels if given
-    x_label <- input$x_label
-    y_label <- input$y_label
+    x_label <- input$x_label_basic
+    y_label <- input$y_label_basic
     
     # Set font size
-    par(cex.lab = input$font_size, cex.axis = input$font_size, cex.main = input$font_size,
+    par(cex.lab = input$font_size_basic, cex.axis = input$font_size_basic, cex.main = input$font_size_basic,
         mar = c(5, 5, 4, 2) + 0.1)
     
-    # Call TACT_fin function with 
-    plot <- TACT_fin(r = input$r, distribution = "normal",
-                     Xlab = bquote(bold(.(x_label))),  # Bold face axis labels
-                     Ylab = bquote(bold(.(y_label))),
-                     font_size = input$font_size,
-                     Cex_multiplier = input$point_size,
-                     language=selected_language())
+    # Call TACT_fun_basic function with 
+    plot <- TACT_fun_basic(r = input$r_basic, distribution = "normal",
+                           Xlab = bquote(bold(.(x_label))),  # Bold face axis labels
+                           Ylab = bquote(bold(.(y_label))),
+                           font_size = input$font_size_basic,
+                           Cex_multiplier = input$point_size_basic,
+                           language = selected_language())
     plot$plot
     
     # Below the plot text 
-    output$text_below_plot <- renderUI({
-      if (input$r >= 0) {
+    output$text_below_plot_basic <- renderUI({
+      if (input$r_basic >= 0) {
         bullet_points <- ""
         
-        if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language != "es") {
+        if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language_basic != "es") {
           bullet_points <- paste(
             "<li>", 
             round(sum(diag(t(apply(plot$crosstabs, 2, rev)))) / sum(plot$crosstabs) * 100, 1),
-                  TACT_languages[TACT_languages$language == input$language, "match_1"],
-                  "</li>",
-                  "<li>",
-                  TACT_languages[TACT_languages$language == input$language, "match_2"],
-                  "</li>",
-                  sep = ""
-            )
-        } else if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language == "es") {
+            TACT_languages[TACT_languages$language == input$language_basic, "match_1"],
+            "</li>",
+            "<li>",
+            TACT_languages[TACT_languages$language == input$language_basic, "match_2"],
+            "</li>",
+            sep = ""
+          )
+        } else if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language_basic == "es") {
           bullet_points <- paste(
             "<li>Keskmiselt ",
             round(sum(diag(t(apply(plot$crosstabs, 2, rev)))) / sum(plot$crosstabs) * 100, 1),
-                  "% väärtustest kattuvad",
-                  "</li>",
-                  "<li>Seose täieliku puudumise korral kattuks 33.3%</li>",
-                  sep = ""
-            )
+            "% väärtustest kattuvad",
+            "</li>",
+            "<li>Seose täieliku puudumise korral kattuks 33.3%</li>",
+            sep = ""
+          )
         }
         
         # Create an unordered list with bullet points
-        HTML(paste0("<ul class='text-below-plot' style='font-size: ",
-                    input$font_size, "em;'>", bullet_points, "</ul>"))
-        
+        HTML(paste0("<ul class='text-below-plot_basic' style='font-size: ",
+                    input$font_size_basic, "em;'>", bullet_points, "</ul>"))
       }
     })
+  })
+  
+  
+  # ADVANCED server
+  selected_language <- reactive({
+    input$language_advanced
+  })
+  
+  output$title_panel_advanced <- renderUI({
+    HTML(paste0("<h1>", translations[[selected_language()]]$title, "</h1>"))
+  })
+  
+  output$slider_label_advanced <- renderText({
+    translations[[selected_language()]]$r_slider
+  })
+  
+  output$x_label_text_advanced <- renderText({
+    translations[[selected_language()]]$name_x
+  })
+  
+  output$y_label_text_advanced <- renderText({
+    translations[[selected_language()]]$name_y
+  })
+  
+  output$font_size_text_advanced <- renderText({
+    translations[[selected_language()]]$font_prompt
+  })
+  
+  output$point_size_text_advanced <- renderText({
+    translations[[selected_language()]]$point_prompt
+  })
+  
+  output$tact_info_text_advanced <- renderText({
+    translations[[selected_language()]]$tact_paper_prompt
+  })
+  
+  output$tactPlot_advanced <- renderPlot({
+    # Use the chosen labels if given
+    x_label <- input$x_label_advanced
+    y_label <- input$y_label_advanced
     
-
+    # Set font size
+    par(cex.lab = input$font_size_advanced, cex.axis = input$font_size_advanced, cex.main = input$font_size_advanced,
+        mar = c(5, 5, 4, 2) + 0.1)
+    
+    # Call TACT_fun_advanced function with 
+    plot <- TACT_fun_basic(r = input$r_advanced, distribution = input$distribution_advanced,
+                           Xlab = bquote(bold(.(x_label))),  # Bold face axis labels
+                           Ylab = bquote(bold(.(y_label))),
+                           n = input$n_advanced,n.plotted = input$n.plotted_advanced,
+                           font_size = input$font_size_advanced,
+                           Cex_multiplier = input$point_size_advanced,
+                           language = selected_language())
+    plot$plot
+    
+    # Below the plot text 
+    output$text_below_plot_advanced <- renderUI({
+      if (input$r_advanced >= 0) {
+        bullet_points <- ""
+        
+        if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language_advanced != "es") {
+          bullet_points <- paste(
+            "<li>", 
+            round(sum(diag(t(apply(plot$crosstabs, 2, rev)))) / sum(plot$crosstabs) * 100, 1),
+            TACT_languages[TACT_languages$language == input$language_advanced, "match_1"],
+            "</li>",
+            "<li>",
+            TACT_languages[TACT_languages$language == input$language_advanced, "match_2"],
+            "</li>",
+            sep = ""
+          )
+        } else if (abs(diff(plot$cutoffsx)) > 0.001 & abs(diff(plot$cutoffsy)) > 0.001 & input$language_advanced == "es") {
+          bullet_points <- paste(
+            "<li>Keskmiselt ",
+            round(sum(diag(t(apply(plot$crosstabs, 2, rev)))) / sum(plot$crosstabs) * 100, 1),
+            "% väärtustest kattuvad",
+            "</li>",
+            "<li>Seose täieliku puudumise korral kattuks 33.3%</li>",
+            sep = ""
+          )
+        }
+        
+        # Create an unordered list with bullet points
+        HTML(paste0("<ul class='text-below-plot_advanced' style='font-size: ",
+                    input$font_size_advanced, "em;'>", bullet_points, "</ul>"))
+      }
+    })
   })
 }
 
